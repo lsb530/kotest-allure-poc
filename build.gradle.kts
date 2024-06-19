e67e8621b079fdf73638a690e1952b36c11b4a03
boki <lsb530@naver.com>
2024-06-20 01:57:49 +0900

    Add kotest and allure-html-reporter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+    id("io.qameta.allure") version "2.11.2"
+    id("io.qameta.allure-report") version "2.11.2"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
 }
@@ -18,6 +20,9 @@ repositories {
     mavenCentral()
 }
 
+val koTestVersion = "5.9.0"
+val koTestAllureExtensionVersion = "1.4.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -25,6 +30,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("io.kotest:kotest-runner-junit5:$koTestVersion")
+    testImplementation("io.kotest:kotest-extensions-htmlreporter:$koTestVersion")
+    implementation("io.kotest.extensions:kotest-extensions-allure:$koTestAllureExtensionVersion")
 }
 
 kotlin {
