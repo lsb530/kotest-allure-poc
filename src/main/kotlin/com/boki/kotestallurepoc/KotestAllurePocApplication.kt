22f9fff68b7a8145c880d7e117cd6aa21567fb8f
boki <lsb530@naver.com>
2024-06-20 01:11:23 +0900

    Initial Commit

---
+++ src/main/kotlin/com/boki/kotestallurepoc/KotestAllurePocApplication.kt
@@ -0,0 +1,11 @@
+package com.boki.kotestallurepoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotestAllurePocApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotestAllurePocApplication>(*args)
+}
---
+++ src/test/kotlin/com/boki/kotestallurepoc/KotestAllurePocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boki.kotestallurepoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotestAllurePocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
